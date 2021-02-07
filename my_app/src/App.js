9ec582a4f056a84e1b614d5f03214ee8a74d5b5b
Sean McClure <seanaaron100@gmail.com>
2021-02-07 04:17:25 +0000

    initial commit

---
+++ my_app/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function MyApp() {
+    return(
+        <div className="wrapper">
+            <div className="item">
+                <img className="logo" src="https://bit.ly/39Xa87U" alt="logo"></img>
+            </div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+        </div>
+    )
+}
+
+export default MyApp;
+
+
+
+
